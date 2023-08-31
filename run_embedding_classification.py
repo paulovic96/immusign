0937a7c5aca7128be60783d57a1f747e10067901
paulovic96 <paul.schmibo@gmail.com>
2023-08-31 08:55:06 +0200

    added classical baseline models to classify emebddings. Option to only use top-n embeddings for classification

--- run_embedding_classification.py
+++ run_embedding_classification.py
@@ -7,6 +7,11 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import StratifiedShuffleSplit, StratifiedKFold
 from sklearn.metrics import accuracy_score, recall_score, precision_score, roc_auc_score, matthews_corrcoef, classification_report
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+import xgboost as xgb
+from catboost import CatBoostClassifier
 import matplotlib.pyplot as plt
 import os
 import json
@@ -91,10 +96,20 @@ def load_data(comparisons, settings):
     
     cloneFraction = []
     for name, group in df.groupby("clones.txt.name"):
-        if settings["embedding_method"] == "sum":
-            rep_embedding = sum(group.cloneCount * group.esm_embedding)
-        elif settings["embedding_method"] == "mean":
-            rep_embedding = np.mean(group.cloneCount * group.esm_embedding)
+        if settings["n_clones"] != "all":
+            group = group.iloc[:settings["n_clones"]]
+        
+        if settings["stack_embeddings"] == False:
+            if settings["embedding_method"] == "sum":
+                rep_embedding = sum(group.cloneCount * group.esm_embedding)
+            elif settings["embedding_method"] == "mean":
+                rep_embedding = np.mean(group.cloneCount * group.esm_embedding)
+            elif settings["embedding_method"] == "prob_weighted_sum":
+                rep_embedding = sum(group.cloneCount * group.esm_embedding * group.olga_pgen_aa)
+            elif settings["embedding_method"] == "prob_weighted_mean":
+                rep_embedding = np.mean(group.cloneCount * group.esm_embedding * group.olga_pgen_aa)
+        else:
+            rep_embedding = np.stack((group.cloneCount * group.esm_embedding).values)
         
         for i, comp in enumerate(comparisons):
             if group.lymphoma_specification.iloc[0] in comp:
@@ -107,18 +122,26 @@ def load_data(comparisons, settings):
             embedded_df.loc[len(embedded_df)] = [name, label, rep_embedding]
         cloneFraction.append(group['cloneFraction'].values[0])
     
-    X = np.concatenate(list(embedded_df["embedding"].apply(lambda x: x.reshape((1,-1)))))
+    if settings["stack_embeddings"] == False:
+        X = np.concatenate(list(embedded_df["embedding"].apply(lambda x: x.reshape((1,-1)))))
+        if settings["add_clonality"]:
+            clonalitys = np.expand_dims(list(embedded_df["clonality"]), axis=1)
+            X = np.hstack((X, clonalitys))
+    else:
+        X = np.asarray(embedded_df["embedding"])
+        if settings["add_clonality"]:
+            clonalitys = list(embedded_df["clonality"])
+            X = np.asarray([np.hstack((x, np.expand_dims(np.repeat(clonalitys[i], len(x)), axis =1))) for i, x in enumerate(X)], dtype="object")
+    
     y = list(embedded_df["label"])
-
-    if settings["add_clonality"]:
-        clonalitys = np.expand_dims(list(embedded_df["clonality"]), axis=1)
-        X = np.hstack((X, clonalitys))
     
     if settings["standardize"]:
-        means = np.mean(X, axis=0)
+        means = np.mean(X, axis=0)  
         stds = np.std(X, axis=0)
-        X = (X-means)/stds
+        X = np.divide(X-means, stds, out=np.zeros_like(X), where=stds!=0)
+        #X = (X-means)/stds
     
+   
     return X, np.asarray(y), np.asarray(cloneFraction)
 
 
@@ -140,8 +163,7 @@ def initialize_model(model_name, settings):
 
 
 
-def train_model(model_name, comparisons, settings, train_index, test_index, types,store_path=None):
-    
+def train_model(model_name, comparisons, settings, train_index, test_index, types,store_path=None): 
     print("Start %s training..." % model_name)
     if store_path == None:
         store_dir  = os.path.join("immusign/outputs_%s" % model_name.replace(" ", ""), _run_name("classification") )
@@ -335,23 +357,176 @@ def train_model(model_name, comparisons, settings, train_index, test_index, type
             target_labels = np.unique(np.concatenate([y_test[low_mask], y_pred[low_mask]]))               
         f.write(classification_report(y_test[low_mask], y_pred[low_mask], target_names=np.asarray(types)[target_labels], zero_division=0))
         mcc = matthews_corrcoef(y_test[low_mask], y_pred[low_mask])
-        f.write(row_fmt_mcc.format("mcc", "", "", mcc, "", width=width, digits=digits))       
+        f.write(row_fmt_mcc.format("mcc", "", "", mcc, "", width=width, digits=digits))   
 
-def hyperopt_classical(iterations, model_name, comparisons, train_index, test_index, types, store_path=None):
-    distributions = dict(
-                        device = ["mps"],
-                        n_splits= [3],
-                        embedding_method = ["sum", "mean"],
-                        input_channel = [320],
-                        output_channel = [len(types)],
-                        hidden_units = [320, 640, 960, 1280],
-                        hidden_layers = [1, 2, 3, 4, 5, 6, 7],
-                        standardize = [False, True],
-                        lr = [1e-3, 1e-4, 1e-5, 1e-6, 1e-7],
-                        n_epochs = [100, 200, 300, 400, 500],
-                        batch_size = [4, 8, 16, 32, 64],
-                        add_clonality = [False, True]
-                        )
+def get_model(model_name, settings):
+        if model_name == 'Random Forest':
+            model = RandomForestClassifier(n_estimators=settings["n_estimators"], max_depth = settings["max_depth"], random_state=42)
+        elif model_name == 'XGBoost':
+            model = xgb.XGBClassifier(n_estimators=settings["n_estimators"], max_depth = settings["max_depth"], random_state=42)
+        elif model_name == 'LightGBM':
+            model = lightgbm.LGBMClassifier(n_estimators=settings["n_estimators"], max_depth = settings["max_depth"], random_state=42, bagging_fraction=1.0, boost_from_average=False, verbose=-1)
+        elif model_name == "CatBoost":
+            model = CatBoostClassifier(n_estimators=settings["n_estimators"], max_depth = settings["max_depth"], random_state=42, verbose=False)
+        elif model_name == "TabPFN":
+            raise NotImplementedError
+        elif model_name == "Logistic Regression":
+            model = LogisticRegression(max_iter=settings["max_iter"])
+        elif model_name == "SVM":
+            model = SVC(max_iter=settings["max_iter"], kernel = settings["kernel"])
+        return model    
+
+def baseline(model_name, comparisons, settings, train_index, test_index, types, store_path):
+    print("Start %s training..." % model_name)
+    if store_path == None:
+        store_dir  = os.path.join("immusign/outputs_%s_baseline" % model_name.replace(" ", ""), _run_name("classification") )
+    else:
+        store_dir = os.path.join(store_path, "outputs_%s_baseline" % model_name.replace(" ", ""), _run_name("classification") )
+
+    os.makedirs(store_dir)
+    with open(os.path.join(store_dir, "settings.json"), 'w') as outfile:
+        json.dump(settings, outfile, indent=2)
+
+    X, y, clone_fractions  = load_data(comparisons, settings)
+
+    X_test = X[test_index]
+    y_test = y[test_index]
+    clone_fractions_test = clone_fractions[test_index]
+    
+    X = X[train_index]
+    y = y[train_index]
+    
+    k_fold = StratifiedKFold(n_splits=settings["n_splits"], shuffle=True, random_state=15)
+    folds = k_fold.split(X, y)
+    
+    # init accuracy, recall, precision, roc_auc, mcc for validation set in one dict
+    accuracies = []
+    recalls = []
+    precisions = []
+    roc_aucs = []
+    mccs = []
+    masked_dlbcl_accuracies = []
+    for k, (train, val) in enumerate(folds):
+      
+        X_train, y_train = X[train], y[train]
+        X_val, y_val = X[val], y[val]
+
+        model = get_model(model_name, settings)
+        model.fit(X_train, y_train)
+
+        # Make predictions on the test data
+        y_pred = model.predict(X_val)
+
+        # Measure the performance of the model using the metrics
+        accuracies.append(accuracy_score(y_val, y_pred))
+        recalls.append(recall_score(y_val, y_pred, average='binary' if len(np.unique(y)) == 2 else 'weighted', zero_division=0.0))
+        precisions.append(precision_score(y_val, y_pred, average='binary' if len(np.unique(y)) == 2 else 'weighted', zero_division=0.0))
+        roc_aucs.append(roc_auc_score(y_val, y_pred) if len(np.unique(y)) == 2 else np.nan)
+        mccs.append(matthews_corrcoef(y_val, y_pred))
+
+        low_cf = np.array([1 if ((clone_fractions[val][i] < 0.2) & (y_val[i] == 1)) else 0 for i in range(len(y_val))])
+        low_mask = (low_cf== 1)
+        if len(y_val[low_mask]) == 0:
+            masked_dlbcl_accuracies.append([np.nan, np.nan, np.nan])
+        else:
+            masked_dlbcl_accuracies.append([accuracy_score(y_val[low_mask],y_pred[low_mask] ), 
+                                precision_score(y_val[low_mask],y_pred[low_mask] , average='binary' if len(np.unique(y)) == 2 else 'weighted', zero_division=0.0), 
+                                recall_score(y_val[low_mask],y_pred[low_mask], average='binary' if len(np.unique(y)) == 2 else 'weighted', zero_division=0.0)])
+
+
+    performance_df = pd.DataFrame({'accuracy': accuracies, 'recall': recalls, 'precision': precisions, 'roc_auc': roc_aucs, 'mcc' : mccs})
+  
+    # store in storedir
+    masked_scores = pd.DataFrame(columns=["low_dlbcl_accuracy", "low_dlbcl_precision", "low_dlbcl_recall"], data = masked_dlbcl_accuracies)
+    performance_df = pd.concat([performance_df, masked_scores], axis=1)
+
+    # add Model and Dataset column
+    performance_df["Model"] = model_name
+    performance_df["Dataset"] = 'Validation'
+    
+
+    # add performance on test set with Dataset 'Test'
+    model = get_model(model_name, settings)
+    model.fit(X, y)
+
+    y_pred = model.predict(X_test)
+    performance_df_test = pd.DataFrame({'accuracy': [accuracy_score(y_test, y_pred)], 'recall': [recall_score(y_test, y_pred, average='binary' if len(np.unique(y)) == 2 else 'weighted', zero_division=0.0)], 'precision': [precision_score(y_test, y_pred, average='binary' if len(np.unique(y)) == 2 else 'weighted', zero_division=0.0)], 'roc_auc': [roc_auc_score(y_test, y_pred) if len(np.unique(y)) == 2 else np.nan], 'mcc' : [matthews_corrcoef(y_test, y_pred)]})
+    performance_df_test["Model"] = model_name
+    performance_df_test["Dataset"] = 'Test'
+
+    # measure performance on low dlbcl test set
+    low_cf = np.array([1 if ((clone_fractions_test[i] < 0.2) & (y_test[i] == 1)) else 0 for i in range(len(y_test))])
+    low_mask = (low_cf== 1)
+    if len(y_test[low_mask]) == 0:
+        masked_dlbcl_accuracies_test = [np.nan,  np.nan,np.nan]
+    else:
+        masked_dlbcl_accuracies_test = [accuracy_score(y_test[low_mask],y_pred[low_mask] ), 
+                            precision_score(y_test[low_mask],y_pred[low_mask] , average='binary' if len(np.unique(y)) == 2 else 'weighted', zero_division=0.0), 
+                            recall_score(y_test[low_mask],y_pred[low_mask], average='binary' if len(np.unique(y)) == 2 else 'weighted', zero_division=0.0)]
+
+    performance_df_test = pd.concat([performance_df_test, pd.DataFrame(columns=["low_dlbcl_accuracy", "low_dlbcl_precision", "low_dlbcl_recall"], data = [masked_dlbcl_accuracies_test])], axis=1)
+    performance_df = pd.concat([performance_df, performance_df_test])
+
+    performance_df.to_csv(os.path.join(store_dir, "performance.csv"), index=False)
+
+    with open(os.path.join(store_dir,'test_scores.txt'), 'w') as f:
+        digits = 2
+        width = len("weighted avg")
+        row_fmt_mcc = (
+                    "{:>{width}s} "
+                    + " {:>9.{digits}}" * 2
+                    + " {:>9.{digits}f}"
+                    + " {:>9.{digits}}\n"
+                )
+        f.write("%s Test Scores\n" % model_name)
+        f.write(classification_report(y_test, y_pred, target_names=np.asarray(types), zero_division=0))  
+        mcc = matthews_corrcoef(y_test, y_pred)
+        f.write(row_fmt_mcc.format("mcc", "", "", mcc, "", width=width, digits=digits))  
+        f.write("\n\n") 
+        f.write("Low dlbcl Scores\n")    
+
+        if model_name == "CatBoost":
+            target_labels = np.unique(np.concatenate([y_test[low_mask], y_pred[low_mask].flatten()]).astype(int))                      
+        else:
+            target_labels = np.unique(np.concatenate([y_test[low_mask], y_pred[low_mask]]))               
+        f.write(classification_report(y_test[low_mask], y_pred[low_mask], target_names=np.asarray(types)[target_labels], zero_division=0))
+        mcc = matthews_corrcoef(y_test[low_mask], y_pred[low_mask])
+        f.write(row_fmt_mcc.format("mcc", "", "", mcc, "", width=width, digits=digits))         
+                        
+
+    
+
+def hyperopt_classical(iterations, model_name, comparisons, train_index, test_index, types, store_path=None, train_baseline=False):
+    if train_baseline:
+        distributions = dict(
+                            n_splits= [3],
+                            embedding_method =["sum", "mean", "prob_weighted_sum", "prob_weighted_mean"],
+                            standardize = [False, True],
+                            add_clonality = [False, True],
+                            stack_embeddings = [False],
+                            n_clones = ["all", 1, 5, 10 ,20, 50],
+                            max_depth=[3, 6, 8, 16],
+                            max_iter = [100, 500, 1000, 5000],
+                            kernel = ['rbf', 'poly', 'sigmoid'],
+                            n_estimators = [100, 200, 400, 800]
+        )
+    else:
+        distributions = dict(
+                            device = ["mps"],
+                            n_splits= [3],
+                            embedding_method =["sum", "mean", "prob_weighted_sum", "prob_weighted_mean"],
+                            input_channel = [320],
+                            output_channel = [len(types)],
+                            hidden_units = [320, 640, 960, 1280],
+                            hidden_layers = [1, 2, 3, 4, 5, 6, 7],
+                            standardize = [False, True],
+                            lr = [1e-3, 1e-4, 1e-5, 1e-6, 1e-7],
+                            n_epochs = [100, 200, 300, 400, 500],
+                            batch_size = [4, 8, 16, 32, 64],
+                            add_clonality = [False, True],
+                            stack_embeddings = [False],
+                            n_clones = ["all", 1, 5, 10 ,20, 50]
+                            )
     
     already_trained_settings = []
     model_path = os.path.join(store_path ,"outputs_%s" % model_name)
@@ -359,7 +534,6 @@ def hyperopt_classical(iterations, model_name, comparisons, train_index, test_in
         for name in files:
             file = os.path.join(path, name)
             if file.endswith("settings.json"):
-                print(file)
                 with open(file) as f:
                     settings = f.read()
                     settings = settings.replace("\n", "").strip()
@@ -385,22 +559,27 @@ def hyperopt_classical(iterations, model_name, comparisons, train_index, test_in
                 if tmp_setting["add_clonality"]:
                     tmp_setting["input_channel"] = 321
                 if tmp_setting not in already_trained_settings:
-                    break    
-        train_model(model_name, comparisons, tmp_setting, train_index, test_index, types, store_path)
+                    break  
+        if train_baseline:
+            baseline(model_name, comparisons, tmp_setting, train_index, test_index, types, store_path)
+        else:
+            train_model(model_name, comparisons, tmp_setting, train_index, test_index, types, store_path)
 
     
 if __name__ == '__main__':
-    store_path = "immusign/results_nlphl_dlbcl_hd/"
-    comparisons = [['nlphl'], ["dlbcl", "gcb_dlbcl", "abc_dlbcl"], ['hd']]
-    comparison_labels = ['nlphl', 'dlbcl', 'hd']
+    store_path = "immusign/results_cll_dlbcl_hd_unspecified_nlphl_thrlbcl_lymphadenitis/"
+    comparisons = [['cll'], ["dlbcl", "gcb_dlbcl", "abc_dlbcl"], ['hd'], ['unspecified'], ['nlphl'], ['thrlbcl'], ['lymphadenitis']]
+    comparison_labels = ['cll', 'dlbcl', 'hd', 'unspecified','nlphl',  'thrlbcl', 'lymphadenitis']
 
-    X, y, clone_fraction = load_data(comparisons, dict(embedding_method = "sum", standardize=True, add_clonality=True))
+    X, y, clone_fraction = load_data(comparisons, dict(embedding_method = "sum", standardize=True, add_clonality=True, n_clones="all", stack_embeddings=False))
     
     sss = StratifiedShuffleSplit(n_splits=1, test_size=0.1, random_state=0)
     train_index, test_index =  sss.split(X, y).__next__()
 
-    hyperopt_classical(20, "Perceptron", comparisons, train_index, test_index, comparison_labels, store_path=store_path)
-    hyperopt_classical(20, "ResNet", comparisons, train_index, test_index, comparison_labels, store_path=store_path)
+    hyperopt_classical(30, "Perceptron", comparisons, train_index, test_index, comparison_labels, store_path=store_path)
+    hyperopt_classical(30, "ResNet", comparisons, train_index, test_index, comparison_labels, store_path=store_path)
+    hyperopt_classical(30, "Logistic Regression", comparisons, train_index, test_index, comparison_labels, store_path=store_path, train_baseline=True)
+    hyperopt_classical(30, "Random Forest", comparisons, train_index, test_index, comparison_labels, store_path=store_path, train_baseline=True)
     
     #store_path = "immusign/results_nlphl_dlbcl_hd/outputs_DNNs/"
     score_to_choose_best = "mcc"
