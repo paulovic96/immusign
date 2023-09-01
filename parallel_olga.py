a3006844bf73fdf475cefa4e80d711ab59f4523d
paulovic96 <paul.schmibo@gmail.com>
2023-09-01 08:47:31 +0200

    updated olga to automatically devide and run on input file

--- parallel_olga.py
+++ parallel_olga.py
@@ -1,6 +1,9 @@
 import subprocess
 import multiprocessing
 import os 
+import argparse
+import pandas as pd
+import numpy as np
 
 def process_chunk(input_file_path, model):
     
@@ -31,21 +34,71 @@ class Copier(object):
         process_chunk(input_file_path, self.model)
 
 def main():
-    input_files = ["Olga_input_igh_1.tsv", "Olga_input_igh_2.tsv", "Olga_input_igh_3.tsv", 
-                   "Olga_input_igh_4.tsv", "Olga_input_igh_5.tsv", "Olga_input_igh_6.tsv",
-                   "Olga_input_igh_7.tsv", "Olga_input_igh_8.tsv"]
+    parser = argparse.ArgumentParser(description="Calculate generation probability for CDR3 sequences according to a generative V(D)J model. Input Table with 3 columns: sequence, Vgen, Jgen")
+    # Add arguments using add_argument method
+    parser.add_argument('--i', type=str, help='Input file')
+    parser.add_argument('--model', type=str, help='Model to use with Olga (e.g. humanIGH)')
 
+
+    args = parser.parse_args()
+    if args.i:
+        input_file = args.i
+    else:
+        print("Please provide a valid input file...")
+    if args.model:
+        model = "--" + args.model
+    else:
+        print("Please prov a valid model specification...")
+
+
+
+    #input_files = ["Olga_input_igh_1.tsv", "Olga_input_igh_2.tsv", "Olga_input_igh_3.tsv", 
+    #               "Olga_input_igh_4.tsv", "Olga_input_igh_5.tsv", "Olga_input_igh_6.tsv",
+    #               "Olga_input_igh_7.tsv", "Olga_input_igh_8.tsv"]
+
+
+    df_to_chunk = pd.read_csv(input_file,sep='\t', header=None)
     num_processes = multiprocessing.cpu_count()  # Number of available CPU cores
-    with multiprocessing.Pool(processes=num_processes) as pool:
-        pool.map(Copier("--humanIGH"), input_files)
+    start_point = 0
+    chunk_length = int(np.ceil(len(df_to_chunk.iloc[start_point:])/num_processes))
+    end_points = np.cumsum([start_point] + [chunk_length for i in range(num_processes)])
 
-    input_files = ["Olga_input_trb_1.tsv", "Olga_input_trb_2.tsv", "Olga_input_trb_3.tsv", 
-                   "Olga_input_trb_4.tsv", "Olga_input_trb_5.tsv", "Olga_input_trb_6.tsv",
-                   "Olga_input_trb_7.tsv", "Olga_input_trb_8.tsv"]
+    chunk_file_name = input_file.split(".")[0]+"_%d.tsv"
 
-    num_processes = multiprocessing.cpu_count()  # Number of available CPU cores
+    input_files = []
+    for i, end_point in enumerate(end_points):
+        if i == 0:
+            continue
+        elif i == len(end_points)-1:
+            df_to_chunk.iloc[end_points[i-1]:].to_csv(chunk_file_name % i, sep="\t", header=False, index=False)
+        else:
+            df_to_chunk.iloc[end_points[i-1]:end_point].to_csv(chunk_file_name % i, sep="\t", header=False, index=False)
+        input_files.append(chunk_file_name % i)
+
+
     with multiprocessing.Pool(processes=num_processes) as pool:
-        pool.map(Copier("--humanTRB"), input_files)
+        pool.map(Copier(model), input_files)
+
+    output_files = [i.replace("input", "output") for i in input_files]
+
+    results = []
+    for o in output_files:
+        olga_results_o = pd.read_csv(o, sep="\t", header=None)
+        results.append(olga_results_o)
+
+    results = pd.concat(results)
+    results.to_csv(input_file.replace("input", "output"), sep="\t", header=False, index=False)
+
+    #input_files = ["Olga_input_trb_1.tsv", "Olga_input_trb_2.tsv", "Olga_input_trb_3.tsv", 
+    #               "Olga_input_trb_4.tsv", "Olga_input_trb_5.tsv", "Olga_input_trb_6.tsv",
+    #               "Olga_input_trb_7.tsv", "Olga_input_trb_8.tsv"]
+
+    #num_processes = multiprocessing.cpu_count()  # Number of available CPU cores
+    #with multiprocessing.Pool(processes=num_processes) as pool:
+    #    pool.map(Copier("--humanTRB"), input_files)
+    for i,f in enumerate(input_files):
+        os.remove(f)
+        os.remove(output_files[i])
 
 if __name__ == "__main__":
     main()
